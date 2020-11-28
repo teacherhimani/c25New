d095bb83bf37446de498c505e9bad3ba7ab1f06f
teacher.himani <teacher.himani@gmail.com>
2020-11-29 00:34:34 +0530

    Another Bird added

--- sketch.js
+++ sketch.js
@@ -29,7 +29,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    bird2 = new Bird(200,200);
 }
 
 function draw(){
@@ -54,4 +54,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    bird2.display();
 }
